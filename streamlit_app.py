d7aef4f9b95668adeb7a98e0f6e695c5724725c4
Jahnavi854 <jahnavi.vurimi@endava.com>
2026-04-14 17:13:23 +0530

    Updated streamlit.app.py
    connected changed needed bc of move rom Sis to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
- 
+cnx=st.connection("snowflake")
+session=cnx.session()
 #st.write(session.get_current_role())
 
