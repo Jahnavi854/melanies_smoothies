140f5ae3f86bd23d64200522edf6eee957c619b2
Jahnavi854 <jahnavi.vurimi@endava.com>
2026-04-15 14:40:18 +0530

    Refactor fruit options retrieval and API request
    Updated the data retrieval process for fruit options and modified the API request to use the search value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,11 @@ conn = st.connection("snowflake")
 session = conn.session()
 
 # Get fruit options
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT_NAME"),col('SEARCH_ON')).to_pandas()
-st.dataframe(data=my_dataframe,use_container_width=True)
-st.stop()
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT_NAME"),col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+
+#st.dataframe(data=my_dataframe,use_container_width=True)
+#st.stop()
 #fruit_list = my_dataframe["FRUIT_NAME"].tolist()
 
 ingredients_list = st.multiselect(
@@ -31,8 +33,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     if st.button("Submit Order"):
